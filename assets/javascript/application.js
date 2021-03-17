3e7e99459dc8159d1e64d841c52befc213fcdd26
Jason Parr <parr.jason.01@gmail.com>
2021-03-16 17:58:55 -0700

    add the orb animation.

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
+/*-------------SPINNING BASEBALL ANIMATION---------------*/
 
 let imgArray = new Array();
 for(i=0; i<24; i++) {
@@ -17,6 +18,32 @@ function rotate() {
     setTimeout(rotate, 30);
 }
 window.addEventListener("load", rotate);
+
+  
+/*-------------POINT TO POINT ORB ANIMATION---------------*/
+
+let currentX = 100;
+let currentY = 100;
+
+function animate () {
+  document.getElementById("orb").style.left = currentX + "px";
+  document.getElementById("orb").style.top = currentY + "px";
+  currentX += 5;
+  currentY += 5;
+  if (currentX > 800 || currentY > 600) {
+    return;
+  }
+  setTimeout(animate, 10);
+}
+ 
+window.addEventListener("load", animate);
+
+
+
+
+
+
+
 
 
 
